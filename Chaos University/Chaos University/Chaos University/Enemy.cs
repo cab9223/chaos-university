a016355a2bb71959a7ade0b6fa406ffa68ce5df0
Christopher Bennett <cab9223@rit.edu>
2014-11-12 17:50:55 -0500

    Tons of changes to Enemy again: Added a Detection Rectangle for hit tests with the player Altered it to move into being in front of the Enemy each time it turns Changed move method to keep the DetectRect moving with it Attack is off but works and defeats the player

--- Chaos University/Chaos University/Chaos University/Enemy.cs
+++ Chaos University/Chaos University/Chaos University/Enemy.cs
@@ -16,17 +16,40 @@ namespace Chaos_University
     {
         public int Difficulty { get; set; }
 
+        private Rectangle detectRect;
+        public Rectangle DetectRect
+        {
+            get
+            {
+                return detectRect;
+            }
+
+            set
+            {
+                detectRect = value;
+            }
+        }
+
 
         public Enemy(int x, int y, int dir, List<Texture2D> textures) //Constructor
             : base(x, y, dir, textures)
         {
             Difficulty = 0;
+
+            if (dir == 0 || dir == 2)
+            {
+                DetectRect = new Rectangle(x + 20, y + 20, (GlobalVar.TILESIZE / 5), (GlobalVar.TILESIZE / 2));
+            }
+            else if (dir == 1 || dir == 3)
+            {
+                DetectRect = new Rectangle(x + 20, y + 20, (GlobalVar.TILESIZE / 2), (GlobalVar.TILESIZE / 5));
+            }
         }
 
 
         public bool Attack(Player obj) //Method for attacking player
         {
-            if (this.CheckCollision(obj) == true)
+            if (obj.PositionRect.Intersects(this.DetectRect) == true)
             {
                 //Maybe add an effect or sound to say the guard caught the player
                 return true;
@@ -53,21 +76,25 @@ namespace Chaos_University
                     {
                         case 0:
                             Turn(2);
+                            DetectRect = new Rectangle(DetectRect.X, DetectRect.Y - 18, (GlobalVar.TILESIZE / 5), (GlobalVar.TILESIZE / 2));
                             //Direction = 2;
                             break;
 
                         case 1:
                             Turn(3);
+                            DetectRect = new Rectangle(DetectRect.X + 18, DetectRect.Y, (GlobalVar.TILESIZE / 2), (GlobalVar.TILESIZE / 5));
                             //Direction = 3;
                             break;
 
                         case 2:
                             Turn(0);
+                            DetectRect = new Rectangle(DetectRect.X, DetectRect.Y + 18, (GlobalVar.TILESIZE / 5), (GlobalVar.TILESIZE / 2));
                             //Direction = 0;
                             break;
 
                         case 3:
                             Turn(1);
+                            DetectRect = new Rectangle(DetectRect.X - 18, DetectRect.Y, (GlobalVar.TILESIZE / 2), (GlobalVar.TILESIZE / 5));
                             //Direction = 1;
                             break;
                     } 
@@ -113,33 +140,47 @@ namespace Chaos_University
                     new Vector2(listTextures[0].Width / 2, listTextures[0].Height / 2),
                     SpriteEffects.None,
                     0.0f);
+
+            //obj.Draw(listTextures[0],
+            //        new Rectangle(DetectRect.X + DetectRect.Width / 2, DetectRect.Y + DetectRect.Height / 2, DetectRect.Width, DetectRect.Height),
+            //        null,
+            //        Color.White,
+            //        (float)(Math.Atan2(Vector.Y, Vector.X) + Math.PI / 2),
+            //        new Vector2(listTextures[0].Width / 2, listTextures[0].Height / 2),
+            //        SpriteEffects.None,
+            //        0.0f);
         }
 
 
         public override void Move(int distance)
         {
             Rectangle temp = PositionRect;
+            Rectangle temp2 = DetectRect;
 
             switch (Direction)
             {
                 case 0:
                     temp.Y += distance;
+                    temp2.Y += distance;
                     break;
 
                 case 1:
                     temp.X -= distance;
+                    temp2.X -= distance;
                     break;
 
                 case 2:
                     temp.Y -= distance;
+                    temp2.Y -= distance;
                     break;
 
                 case 3:
                     temp.X += distance;
+                    temp2.X += distance;
                     break;
             }
-
             PositionRect = temp;
+            DetectRect = temp2;
         }
 
 
@@ -159,6 +200,7 @@ namespace Chaos_University
                 case 2:
                     Vector = new Vector2(0, -1);
                     Direction = 2;
+
                     break;
                 case 3:
                     Vector = new Vector2(1, 0);
