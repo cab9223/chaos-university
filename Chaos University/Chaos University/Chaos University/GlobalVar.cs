4de7355c741c85a69fb94ccda7b9c29d34c503f7
Alex Bronner <acbronn@gmail.com>
2014-11-17 05:02:46 -0500

    Returned sizing to normal. Enabled music again.

--- Chaos University/Chaos University/Chaos University/GlobalVar.cs
+++ Chaos University/Chaos University/Chaos University/GlobalVar.cs
@@ -21,7 +21,7 @@ namespace Chaos_University
         public static int LevelCount = 5;
 
         // Allows us to edit the size of a single tile- also useful for a bunch of other things that have been startedo n.
-        public static int TILESIZE = 25;
+        public static int TILESIZE = 50;
 
         //Colors that CaracterCreator edits, set to white(no tint) by default
         public static Int16[] ColorsSplit = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
