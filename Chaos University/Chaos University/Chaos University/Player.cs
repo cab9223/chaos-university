ab5839e628d69d776d137d25a4a91bc7461897dd
Ian Cavanaugh <flameian@mac.com>
2014-12-03 17:40:14 -0500

    Fussing with trying to center the sword around the player's movement.

--- Chaos University/Chaos University/Chaos University/Player.cs
+++ Chaos University/Chaos University/Chaos University/Player.cs
@@ -172,7 +172,10 @@ namespace Chaos_University
 
                 if (AbilityActive && ThisMajor == Major.Ninja)
                 {
-                    this.ThisGear.PositionRect = new Rectangle(this.PositionRect.X, this.PositionRect.Y, this.ThisGear.PositionRect.Width, this.ThisGear.PositionRect.Height);
+                    this.ThisGear.PositionRect = new Rectangle(this.PositionRect.X - this.PositionRect.Width / 2,
+                        this.PositionRect.Y - this.PositionRect.Height / 2,
+                        this.ThisGear.PositionRect.Width,
+                        this.ThisGear.PositionRect.Height);
                 }
             }
         }
